80cbaaf5ce1a3a145669b16d39128f362f407bb1
Murad Nurmukhamedov <mns130691@gmail.com>
2020-10-08 09:44:23 +0300

    append each 3rd row from list1 to list2

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__. "/vendor/autoload.php";
+
+$client = new Google_Client();
+$client->setAuthConfig(__DIR__.'/credentials.json');
+$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+
+$service = new Google_Service_Sheets($client);
+
+$spreadsheetId = '1FS5_xR8YqkBnQPBtL2-r5ieNik-QOVILke5GWnxHovM';
+$params = [
+  'includeGridData'=>true
+];
+
+$response = $service->spreadsheets->get($spreadsheetId, $params);
+
+$values = array();
+
+$rows = $response->getSheets()[0]->data[0]->rowData;
+
+for ($i=2; $i < count($rows); $i = $i+3) { 
+
+    $data = $rows[$i]->values;
+
+    foreach($data as $row){
+
+      $values[0][] = $row->formattedValue;
+    // break;
+
+    }
+    
+    $requestBody = new Google_Service_Sheets_ValueRange(['values' => $values]);
+    $options = array( 'valueInputOption' => 'RAW' );
+    if($service->spreadsheets_values->append( $spreadsheetId, 'List2', $requestBody, $options )) echo 1;
+
+    $values = array();
+  // break;
+
+}
